0dc87331feda1fb3560ab25f83a362247be2c63f
Mohsen.Biglari <mohsen.biglari@Wolt.com>
2023-12-09 20:14:52 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,62 @@
+import kotlin.math.min
+
 fun main() {
+
+    val otherFormsOfDigits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+    fun String.findAMatch(startIndex: Int, set: List<String>): Int {
+        set.forEachIndexed { index, d ->
+            if (substring(startIndex, min(this.length, startIndex + d.length)) == d) {
+                return index + 1
+            }
+        }
+        return -1
+    }
+
+    fun String.matchADigit(index: Int, c: Char): Int {
+        if (c.isDigit()) return c.digitToInt()
+        return findAMatch(index, otherFormsOfDigits)
+    }
+
+    fun String.firstDigit(): Int {
+        forEachIndexed { index, c ->
+            val match = matchADigit(index, c)
+            if (match >= 0) return match
+        }
+        error("Invalid input")
+    }
+
+    fun String.lastDigit(): Int {
+        forEachIndexedReversed { index, c ->
+            val match = matchADigit(index, c)
+            if (match >= 0) return match
+        }
+        error("Invalid input")
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { acc, line ->
+            val firstDigit = line.first { it.isDigit() }.digitToInt()
+            val lastDigit = line.last { it.isDigit() }.digitToInt()
+
+            acc + firstDigit * 10 + lastDigit
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { acc, line ->
+            val firstDigit = line.firstDigit()
+            val lastDigit = line.lastDigit()
+
+            acc + firstDigit * 10 + lastDigit
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01_test_part1")
+    check(part1(testInput1) == 142)
+
+    val testInput2 = readInput("Day01_test_part2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+inline fun String.forEachIndexedReversed(action: (index: Int, c: Char) -> Unit) {
+    for (index in lastIndex downTo 0) action(index, this[index])
+}
